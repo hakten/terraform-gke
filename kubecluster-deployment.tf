d7b3129b4beba931b81876171c066657056083bc
Huseyin Akten <husakten@gmail.com>
2020-03-15 23:46:30 -0500

    yes

--- kubecluster-deployment.tf
+++ kubecluster-deployment.tf
@@ -1,18 +1,18 @@
 provider "google" {
-  # credentials = "${file("./fuchicorp-service-account.json")}"
+  credentials = "${file("./fuchicorp-service-account.json")}"
   project     = "${var.google_project_id}"
 }
 
-resource "google_container_cluster" "cluster" {
-  name               = "${var.cluster_name}"
-  network            = "default"
-  subnetwork         = "default"
-  location           = "us-central1-a"
-  min_master_version = "${var.cluster_version}"
-  initial_node_count = "${var.node_count}"
-  project            = "${var.google_project_id}"
+# resource "google_container_cluster" "cluster" {
+#   name               = "${var.cluster_name}"
+#   network            = "default"
+#   subnetwork         = "default"
+#   location           = "us-central1-a"
+#   min_master_version = "${var.cluster_version}"
+#   initial_node_count = "${var.node_count}"
+#   project            = "${var.google_project_id}"
 
-  node_config {
-    machine_type = "n1-standard-2"
-  }
-}
+#   node_config {
+#     machine_type = "n1-standard-2"
+#   }
+# }
