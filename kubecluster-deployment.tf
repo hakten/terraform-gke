2a988ae2948aeb5313e940ad5fe9b633ffd6b630
Huseyin Akten <husakten@gmail.com>
2020-03-15 23:57:07 -0500

    asdfas

--- kubecluster-deployment.tf
+++ kubecluster-deployment.tf
@@ -3,16 +3,16 @@ provider "google" {
   project     = "${var.google_project_id}"
 }
 
-# resource "google_container_cluster" "cluster" {
-#   name               = "${var.cluster_name}"
-#   network            = "default"
-#   subnetwork         = "default"
-#   location           = "us-central1-a"
-#   min_master_version = "${var.cluster_version}"
-#   initial_node_count = "${var.node_count}"
-#   project            = "${var.google_project_id}"
+resource "google_container_cluster" "cluster" {
+  name               = "${var.cluster_name}"
+  network            = "default"
+  subnetwork         = "default"
+  location           = "us-central1-a"
+  min_master_version = "${var.cluster_version}"
+  initial_node_count = "${var.node_count}"
+  project            = "${var.google_project_id}"
 
-#   node_config {
-#     machine_type = "n1-standard-2"
-#   }
-# }
+  node_config {
+    machine_type = "n1-standard-2"
+  }
+}
