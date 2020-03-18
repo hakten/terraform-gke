b31645f056fcf3409b4328214179d880646c0aa6
Huseyin Akten <husakten@gmail.com>
2020-03-18 16:49:44 -0500

    yes

--- kubecluster-deployment.tf
+++ kubecluster-deployment.tf
@@ -20,6 +20,7 @@ resource "google_container_cluster" "cluster" {
 
  resource "null_resource" "get_kubectl" {
     provisioner "local-exec" {
+    when    = "create"
     command = "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl"
   }
 }
@@ -27,6 +28,7 @@ resource "google_container_cluster" "cluster" {
  resource "null_resource" "chmod_kubectl" {
  depends_on = ["null_resource.get_kubectl"]
     provisioner "local-exec" {
+    when    = "create"  
     command = "sudo chmod +x kubectl"
   }
 }
@@ -34,6 +36,7 @@ resource "google_container_cluster" "cluster" {
  resource "null_resource" "move_kubectl" {
  depends_on = ["null_resource.chmod_kubectl"]
     provisioner "local-exec" {
+    when    = "create"  
     command = "mv kubectl /bin"
   }
 }
